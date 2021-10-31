43abdf24a4125b1a625f0609dad858a7f70acf5c
Vadym Barabanov <introvert.png@gmail.com>
2021-10-31 18:47:04 +0200

    configure port from env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+const DEFAULT_PORT = 4000
+const PORT = process.env.PORT || DEFAULT_PORT
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  await app.listen(PORT, () =>
+    console.log(`🚀 Server started on port http://localhost:${PORT}`),
+  )
 }
-bootstrap();
+bootstrap()
