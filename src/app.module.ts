3b6c7ce521b61a4b0111e1f3fb29d357a4ea50a4
Vadym Barabanov <introvert.png@gmail.com>
2021-11-02 22:28:35 +0200

    add users entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { __prod__ } from './constants/system.constant'
+import { Users } from './users/users.entity'
 import { UsersModule } from './users/users.module'
 
 @Module({
@@ -16,7 +17,7 @@ import { UsersModule } from './users/users.module'
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      entities: [],
+      entities: [Users],
       synchronize: !__prod__,
     }),
     UsersModule,
