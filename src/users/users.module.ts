28105df55f99c1d48fdfc262d35a6f2aa7ed5561
Vadym Barabanov <introvert.png@gmail.com>
2021-11-03 23:57:11 +0200

    add users repository to users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { Module } from '@nestjs/common'
+import { UsersService } from './users.service'
+import { UsersController } from './users.controller'
+import { UsersRepository } from './users.repository'
 
 @Module({
-  providers: [UsersService],
-  controllers: [UsersController]
+  providers: [UsersService, UsersRepository],
+  controllers: [UsersController],
 })
 export class UsersModule {}
